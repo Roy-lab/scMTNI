25234d385de13665fc9a79bd450115d774f9478c
shiluzhang <szhang256@wisc.edu>
2022-08-21 23:34:42 -0500

    Commit message

--- Scripts/Evaluation/aupr_wrapper_list_intersection.sh
+++ Scripts/Evaluation/aupr_wrapper_list_intersection.sh
@@ -5,11 +5,11 @@ outdir=$3
 prefix=$4
 regulators=$5
 targets=$6
-python /mnt/dv/wid/projects2/Roy-regnet-inference/CVN/scripts/aupr/prepareAUClistfile_intersect.py --truefile $Truefile --predfile $Predfile --outdir $outdir --prefix $prefix --regulators $regulators --targets $targets
+python Scripts/Evaluation/prepareAUClistfile_intersect.py --truefile $Truefile --predfile $Predfile --outdir $outdir --prefix $prefix --regulators $regulators --targets $targets
 
 listfile=${outdir}/${prefix}_list.txt
 outfile=${outdir}/${prefix}
-AUC_jar=/mnt/dv/wid/projects5/Roy-singlecell/shilu_work/programs/AUCCalculator/auc.jar
+AUC_jar=Scripts/Evaluation/auc.jar
 echo java -jar ${AUC_jar} ${listfile} list
 java -jar ${AUC_jar} ${listfile} list > $outfile
 tail -n 2 $outfile > ${outdir}/${prefix}.out.txt
